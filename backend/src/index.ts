175c0628b3da99a7425bfae3b7887e561ff95c9e
ibrariqbal02 <ibrar.iqbel@gmail.com>
2026-08-28 01:54:19 +0500

    add the datase setup and add the all model

---
+++ backend/src/index.ts
@@ -0,0 +1,42 @@
+import "dotenv/config";
+import express, { Application } from "express";
+import cors from "cors";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
+import connectDatabase from "./config/db";
+
+const app:Application = express();
+// middleware
+connectDatabase()
+app.use(helmet());
+const PORT = process.env.PORT || 5000;
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
+
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000,
+    limit: 100,
+    standardHeaders: "draft-8",
+    legacyHeaders: false,
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+ 
+
+// routes
+app.get("/api/health", (_req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Solar Business Platform API is running",
+  });
+});
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
